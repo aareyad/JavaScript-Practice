f75c41c1c76c595f6cac53191dc51e99fd383379
Ali Akbar Reyad <aliakbar.reyad@gmail.com>
2021-01-21 00:58:21 +0600

    Reverse a array without build-in function

---
+++ Array/reverse.js
@@ -0,0 +1,17 @@
+/** 
+  * Task description: Write a method that reverts input array 
+  * Expected Result: [1, 2, 3] => [3, 2, 1] 
+  * Task Complexity: 1 of 5 
+  * @param {Array} array - Array of any elements 
+  * @returns {Array} 
+*/
+const reverse = ( data ) => {
+	const array = [];
+	let length = data.length;
+	while( length ) {
+		array.push( data[--length] );
+	}
+	return array;
+}
+const data = [ 1, 2, 3, 1, 5, 6 ];
+console.log( reverse( data ) );
